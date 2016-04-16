aab76ce41d08d51ab76ef1793f847840a43f62ce
Nikita Babanin <nikitared@iwtlab.net>
2016-04-17 01:40:06 +0700

    add travis configuration. update

--- plugins/codegen-templator-plugin/src/main/java/ru/babaninnv/codegen/plugin/templator/utils/PluginConfiguration.java
+++ plugins/codegen-templator-plugin/src/main/java/ru/babaninnv/codegen/plugin/templator/utils/PluginConfiguration.java
@@ -1,14 +1,12 @@
 package ru.babaninnv.codegen.plugin.templator.utils;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.yaml.snakeyaml.Yaml;
-
-import java.io.File;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by BabaninN on 31.03.2016.
